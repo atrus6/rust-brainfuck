eb1dc8881264fbd7d9c1fee7ff4c3fdc54e67e68
Tim Butram <atrus6@gmail.com>
2013-06-03 17:29:08 +0000

    Completed BrainfucIntrepreter

--- brainfuck.rs
+++ brainfuck.rs
@@ -1,26 +1,58 @@
 struct Program {
-    data: ~str
+    program: ~str,
+    data: ~[int],
 }
 
 impl Program {
-    fn new(data: ~str) -> Program {
-        Program{data:data}
+    fn new(program: ~str) -> Program {
+        Program{program:program, data:~[0, ..30000]}
     }
+}
 
-    fn execute(&self) -> ~str {
-        let mut i = 0u;
+fn execute(mut p: Program) {
+    let mut data_pointer = 0u;
+    let mut ip = 0u;
+    let mut left = 0u;
 
-        while i < str::len(self.data) {
-            let str::CharRange {ch, next} = str::char_range_at(self.data, i);
-            io::println(fmt!("%c", ch));
-            i = i+1;
+    while ip < str::len(p.program) {
+        let str::CharRange {ch, next} = str::char_range_at(p.program, ip);
+
+        match ch {
+            '>' => {
+                if(data_pointer == p.data.len()) {
+                    p.data.push(0)
+                }
+                 data_pointer += 1;
+             },
+            '<' => {
+                if(data_pointer > 0) {
+                    data_pointer -= 1;
+                }
+             },
+            '+' => {
+                p.data[data_pointer] += 1;
+             },
+            '-' => {
+                p.data[data_pointer] -= 1;
+             },
+            '.' => print(fmt!("%c", p.data[data_pointer] as char)),
+            ',' => println("Reading Not Supported"),
+            '[' => {
+                left = ip-1;
+             },
+            ']' => {
+                if(p.data[data_pointer] != 0) {
+                   ip = left;
+                }
+             },
+            _   => println("Ignored")
         }
 
-        return self.data.clone();
+        ip += 1;
     }
 }
 
 fn main() {
-    let u1 = Program::new(~"Test");
-    u1.execute();
+    let mut u1 = Program::new(~"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
+    execute(u1);
 }
